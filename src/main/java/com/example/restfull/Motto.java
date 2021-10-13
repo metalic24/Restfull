49a9a7fd1cc2e0a973c7c20a0e2a53bf84d8c80f
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-10-13 10:09:49 +0200

    proba tekstu

--- src/main/java/com/example/restfull/Motto.java
+++ src/main/java/com/example/restfull/Motto.java
@@ -4,15 +4,35 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Path("/motto")
 public class Motto {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello() throws IOException {
 
+        String filePath = "/pliki/motta.txt";
+        String linia = "Witaj";
+        BufferedReader fileReader = null;
 
+        try {
+            fileReader = new BufferedReader(new FileReader(filePath));
+            linia = fileReader.readLine();
 
-        return "Hello word";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
+
+        return linia;
     }
 }
