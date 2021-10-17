04a0376c5985fca65ebfa9c0c751c0c2b7fdeaf2
metalic24 <65673563+metalic24@users.noreply.github.com>
2021-10-17 15:40:29 +0200

    json dziala

--- src/main/java/com/example/restfull/Quiz.java
+++ src/main/java/com/example/restfull/Quiz.java
@@ -1,25 +1,24 @@
 package com.example.restfull;
 
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 
 @Path("/quiz")
 public class Quiz {
 
+    //TODO sciezka wzgledna
+    File file = new File("C:\\apps\\Restfull\\src\\main\\resources\\pliki\\quiz.json");
+
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws IOException {
+    @Produces(MediaType.APPLICATION_JSON)
+    public File hello() throws IOException {
 
 
-        return "hellow world";
+        return file;
     }
 }
